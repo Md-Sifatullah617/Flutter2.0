c74bd75a85bf4d632cf1d756933d2c53af442113
Md. Sifatullah <sifatullah35-617@diu.edu.bd>
2023-02-16 10:10:49 +0600

    sf

---
+++ lib/show_details.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DetailsOnTap extends StatelessWidget {
+  const DetailsOnTap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Details page"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+            
+        ],
+      ),
+    );
+  }
+}
